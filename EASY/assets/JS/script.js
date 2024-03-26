0039a7c195ba95c61ef13798e6959fb5ed8ebac4
Sapakuchi2020 <152281596+Sapakuchi2020@users.noreply.github.com>
2024-03-27 00:48:35 +0800

    Update script.js

---
+++ EASY/assets/JS/script.js
@@ -0,0 +1,74 @@
+let currMoleTile;
+let currPlantTile;
+let score = 0;
+let gameOver = false;
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    for (let i = 0; i < 4; i++) {
+        let tile = document.createElement("div");
+        tile.id = i.toString();
+        tile.addEventListener("click", selectTile);
+        document.getElementById("board").appendChild(tile);
+    }
+    setInterval(setMole, 890);
+    setInterval(setPlant, 1190);
+}
+
+function getRandomTile() {
+    let num = Math.floor(Math.random() * 4);
+    return num.toString();
+}
+
+function setMole() {
+    if (gameOver) {
+        return;
+    }
+    if (currMoleTile) {
+        currMoleTile.innerHTML = "";
+    }
+    let mole = document.createElement("img");
+    mole.src = "assets/alien.png";
+
+    let num = getRandomTile();
+    if (currPlantTile && currPlantTile.id == num) {
+        return;
+    }
+    currMoleTile = document.getElementById(num);
+    currMoleTile.appendChild(mole);
+}
+
+function setPlant() {
+    if (gameOver) {
+        return;
+    }
+    if (currPlantTile) {
+        currPlantTile.innerHTML = "";
+    }
+    let plant = document.createElement("img");
+    plant.src = "assets/bomb.png";
+
+    let num = getRandomTile();
+    if (currMoleTile && currMoleTile.id == num) {
+        return;
+    }
+    currPlantTile = document.getElementById(num);
+    currPlantTile.appendChild(plant);
+}
+
+function selectTile() {
+    if (gameOver) {
+        return;
+    }
+    if (this == currMoleTile) {
+        score += 10;
+        document.getElementById("score").innerText = score.toString();
+    }
+    else if (this == currPlantTile) {
+        document.getElementById("score").innerText = "GAME OVER: " + score.toString();
+        gameOver = true;
+    }
+}
